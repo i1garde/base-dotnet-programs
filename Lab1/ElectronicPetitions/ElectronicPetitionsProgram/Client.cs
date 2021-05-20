c38c255b0739a2bd69b4c67d886fec850e8d46f4
Ivan <vanyakomarenko@gmail.com>
2021-05-20 14:37:09 +0300

    Client.cs and Moderator.cs classes created

--- Lab1/ElectronicPetitions/ElectronicPetitionsProgram/Client.cs
+++ Lab1/ElectronicPetitions/ElectronicPetitionsProgram/Client.cs
@@ -1,7 +1,15 @@
-﻿namespace ElectronicPetitionsProgram
+﻿using System;
+
+namespace ElectronicPetitionsProgram
 {
     public class Client
     {
-        
+        public int Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
     }
 }
--- Lab1/ElectronicPetitions/ElectronicPetitionsProgram/Moderator.cs
+++ Lab1/ElectronicPetitions/ElectronicPetitionsProgram/Moderator.cs
@@ -2,6 +2,9 @@
 {
     public class Moderator
     {
-        
+        public int Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Patronymic { get; set; }
     }
 }

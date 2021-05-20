43604c3f995b76ad0a84a795cb0f7bc4deecda8b
Ivan <vanyakomarenko@gmail.com>
2021-05-20 14:40:41 +0300

    PetitionService.cs class created

--- Lab1/ElectronicPetitions/ElectronicPetitionsProgram/PetitionService.cs
+++ Lab1/ElectronicPetitions/ElectronicPetitionsProgram/PetitionService.cs
@@ -1,7 +1,21 @@
-﻿namespace ElectronicPetitionsProgram
+﻿using System.Collections.Generic;
+
+namespace ElectronicPetitionsProgram
 {
     public class PetitionService
     {
-        
+        private Client _client;
+        private Category _category;
+        public List<Petition> Petitions;
+
+        void FindPetition(Petition petition)
+        {
+            
+        }
+
+        void AddPetition(Petition petition)
+        {
+            
+        }
     }
 }

9d114150ecb9e50c48d0c101d39cfc3767fe6d03
Ivan <vanyakomarenko@gmail.com>
2021-05-20 15:34:28 +0300

    create 3 instances of test petition class

--- Lab1/ElectronicPetitions/ElectronicPetitionsProgram/Program.cs
+++ Lab1/ElectronicPetitions/ElectronicPetitionsProgram/Program.cs
@@ -6,7 +6,32 @@ namespace ElectronicPetitionsProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Program starts!");
+
+            Petition petition1 = new Petition()
+            {
+                Id = 1,
+                Name = "First Petition",
+                Description = "Description 1",
+                CreationData = DateTime.Now,
+                SubscribersAmount = 0
+            };
+            Petition petition2 = new Petition()
+            {
+                Id = 2,
+                Name = "Second Petition",
+                Description = "Description 2",
+                CreationData = DateTime.Now,
+                SubscribersAmount = 0
+            };
+            Petition petition3 = new Petition()
+            {
+                Id = 3,
+                Name = "Third Petition",
+                Description = "Description 3",
+                CreationData = DateTime.Now,
+                SubscribersAmount = 0
+            };
         }
     }
 }

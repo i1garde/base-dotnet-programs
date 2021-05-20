acb1604f6446a9a4762c04328c19fdc164948ecf
Ivan <vanyakomarenko@gmail.com>
2021-05-20 14:33:00 +0300

    petition and category classes created

--- Lab1/ElectronicPetitions/ElectronicPetitionsProgram/Category.cs
+++ Lab1/ElectronicPetitions/ElectronicPetitionsProgram/Category.cs
@@ -1,7 +1,11 @@
-﻿namespace ElectronicPetitionsProgram
+﻿using System.Collections.Generic;
+
+namespace ElectronicPetitionsProgram
 {
     public class Category
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Petition> Petitions { get; set; }
     }
 }
--- Lab1/ElectronicPetitions/ElectronicPetitionsProgram/Petition.cs
+++ Lab1/ElectronicPetitions/ElectronicPetitionsProgram/Petition.cs
@@ -1,7 +1,14 @@
-﻿namespace ElectronicPetitionsProgram
+﻿using System;
+using System.Data;
+
+namespace ElectronicPetitionsProgram
 {
     public class Petition
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationData { get; set; }
+        public int SubscribersAmount { get; set; }
     }
 }

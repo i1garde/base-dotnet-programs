a797de27ea39137fc88ac23e044c4ab804954e04
Ivan <vanyakomarenko@gmail.com>
2021-05-26 16:52:12 +0300

    finish lab8

--- Lab8/ElectronicPetitionLab8/ElectronicPetitionProgramLab8/FileReader.cs
+++ Lab8/ElectronicPetitionLab8/ElectronicPetitionProgramLab8/FileReader.cs
@@ -1,34 +1,108 @@
-﻿using System.Threading;
+﻿using System;
+using System.IO;
+using System.Threading;
 
 namespace ElectronicPetitionProgramLab8
 {
     public class FileReader
     {
-        public string Text { get; set; }
-        static object locker = new object();
-
-        public FileReader(string text)
+        private readonly string _path;
+        static readonly object obj = new object();
+        static Mutex mutexObj = new Mutex();
+        static Semaphore sem = new Semaphore(1, 1);
+        public FileReader(string path)
         {
-            Text = text;
-        }
-
-        public string GetText()
-        {
-            return Text;
+            _path = path;
         }
         
-        public void AddText()
+        public void WriteFile()
         {
-            Text += $"Added Text {Thread.CurrentThread.Name} ";
-            
-        }
-        
-        public void SynthronizedAddText()
-        {
-            lock (locker)
+            int n = 10;
+            Console.WriteLine(Thread.CurrentThread.Name);
+            try
             {
-                Text += $"Added Text {Thread.CurrentThread.Name} ";
-                Thread.Sleep(100);
+                StreamWriter sw = new StreamWriter(_path, append: true);
+                for (int i = 0; i < n; i++)
+                {
+                    sw.WriteLine($"Thread Name: {Thread.CurrentThread.Name}, Line {i}");
+                } 
+            }
+            catch (System.IO.IOException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+        public void WriteFileSync()
+        {
+            lock (obj)
+            {
+                int n = 10;
+                Console.WriteLine(Thread.CurrentThread.Name);
+                using (StreamWriter sw = new StreamWriter(_path, append: true))
+                {
+                    for (int i = 0; i < n; i++)
+                        sw.WriteLine($"Thread Name: {Thread.CurrentThread.Name}, Line {i}");
+                    
+                    Console.WriteLine($"Wrote, Thread: {Thread.CurrentThread.Name}.");
+                }
+
+                using (StreamReader sr = new StreamReader(_path))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+
+                    Console.WriteLine($"Read, Thread: {Thread.CurrentThread.Name}.");
+                }
+                Console.WriteLine(Thread.CurrentThread.Name);
+            }
+        }
+        public void WriteFileMutex()
+        {
+            mutexObj.WaitOne();
+            {
+                int n = 10;
+                using (StreamWriter sw = new StreamWriter(_path, append: true))
+                {
+                    for (int i = 0; i < n; i++)
+                        sw.WriteLine($"Thread Name: {Thread.CurrentThread.Name}, Line {i}");
+
+                    Console.WriteLine($"Wrote, Thread: {Thread.CurrentThread.Name}.");
+                }
+                using (StreamReader sr = new StreamReader(_path))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                    Console.WriteLine($"Read, Thread: {Thread.CurrentThread.Name}.");
+                }
+            }
+            mutexObj.ReleaseMutex();
+        }
+        
+        public void WriteFileSemaphore()
+        {
+            sem.WaitOne();
+            {
+                int n = 10;
+                using (StreamWriter sw = new StreamWriter(_path))
+                {
+                    for (int i = 0; i < n; i++)
+                        sw.WriteLine($"Thread Name: {Thread.CurrentThread.Name}, Line {i}");
+
+                    Console.WriteLine($"");
+                }
+
+                using (StreamReader sr = new StreamReader(_path))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                    Console.WriteLine($"Read, Thread: {Thread.CurrentThread.Name}.");
+                }
+            }
+            sem.Release();
+        }
+        public void ReadFile(object o)
+        {
+            using (StreamReader sr = new StreamReader(_path))
+            {
+                Console.WriteLine(sr.ReadToEnd());
             }
         }
     }
--- Lab8/ElectronicPetitionLab8/ElectronicPetitionProgramLab8/Program.cs
+++ Lab8/ElectronicPetitionLab8/ElectronicPetitionProgramLab8/Program.cs
@@ -6,11 +6,6 @@ namespace ElectronicPetitionProgramLab8
 {
     class Program
     {
-        public static string textFile = "";
-        
-        static object locker = new object();
-        
-        static Mutex mutexObj = new Mutex();
         static void Main(string[] args)
         {
             int switcher = 0;
@@ -46,53 +41,83 @@ namespace ElectronicPetitionProgramLab8
                 }
                 case 2:
                 {
-                    Console.WriteLine("Помилка в синхронiзацiї потокiв:");//(50/50)
-                    
-                    for (int i = 0; i < 5; i++)
+                    Console.WriteLine("1 - error, 2 - synced");
+                    string num = Console.ReadLine();
+                    int a = Convert.ToInt32(num);
+                    switch (a)
                     {
-                        Thread secondThread = new Thread(new ThreadStart(SynthronizedAddFileText));
-                        secondThread.Name = "secondThread";
-                        secondThread.Start();
-                    
-                        Thread thirdThread = new Thread(new ThreadStart(SynthronizedAddFileText));
-                        thirdThread.Name = "thirdThread";
-                        thirdThread.Start();
-                        
+                        case 1:
+                        {
+                            //
+                            Console.WriteLine("З помилкою");
+                            string path = @"D:\Prog Proj\C#\base-dotnet-programs\Lab8\ElectronicPetitionLab8\ElectronicPetitionProgramLab8\Resources\text.txt";
+                            FileReader instance = new FileReader(path);
+
+                            ThreadStart Thread1 = new ThreadStart(instance.WriteFile);
+                            Thread thread1 = new Thread(Thread1) {Name = "1"};
+                            thread1.Start();
+
+                            ThreadStart Thread2 = new ThreadStart(instance.WriteFile);
+                            Thread thread2 = new Thread(Thread2) {Name = "2"};
+                            thread2.Start();
+
+                            break;
+                        }
+                        case 2:
+                        {
+                            //Синхр
+                            Console.WriteLine("Синхронізуйте  потоки, щоб цієї помилки більше не було.");
+                            string path2 = @"D:\Prog Proj\C#\base-dotnet-programs\Lab8\ElectronicPetitionLab8\ElectronicPetitionProgramLab8\Resources\synctext.txt";
+                            FileReader instance2 = new FileReader(path2);
+
+                            ThreadStart Thread3 = new ThreadStart(instance2.WriteFileSync);
+                            Thread thread3 = new Thread(Thread3) {Name = "1"};
+                            thread3.Start();
+
+                            ThreadStart Thread4 = new ThreadStart(instance2.WriteFileSync);
+                            Thread thread4 = new Thread(Thread4) {Name = "2"};
+                            thread4.Start();
+
+                            Console.WriteLine("Press enter");
+                            Console.ReadLine();
+                            break;
+                        }
                     }
-                    Console.WriteLine(textFile);
-                    textFile = "";
-                    Console.WriteLine("Синхронiзованi потоки:");
-                    for (int i = 0; i < 5; i++)
-                    {
-                        Thread secondThread = new Thread(new ThreadStart(AddFileText));
-                        secondThread.Name = "secondThread";
-                        secondThread.Start();
                     
-                        Thread thirdThread = new Thread(new ThreadStart(AddFileText));
-                        thirdThread.Name = "thirdThread";
-                        thirdThread.Start();
-                        
-                    }
-                    Console.WriteLine(textFile);
-                    textFile = "";
+                    
                     break;
                 }
                 case 3:
                 {
-                    Console.WriteLine("Мютекси:");
-                    for (int i = 0; i < 2; i++)
-                    {
-                        Thread myThread = new Thread(MutexAddFileText);
-                        myThread.Name = $"Поток {i}";
-                        myThread.Start();
-                    }
-                    Console.WriteLine(textFile);
-                    textFile = "";
+                    string path = @"D:\Prog Proj\C#\base-dotnet-programs\Lab8\ElectronicPetitionLab8\ElectronicPetitionProgramLab8\Resources\mutextext.txt";
+                    FileReader instance = new FileReader(path);
+
+                    ThreadStart Thread1 = new ThreadStart(instance.WriteFileMutex);
+                    Thread thread1 = new Thread(Thread1) {Name = "1"};
+                    thread1.Start();
+
+                    ThreadStart Thread2 = new ThreadStart(instance.WriteFileMutex);
+                    Thread thread2 = new Thread(Thread2) {Name = "2"};
+                    thread2.Start();
+
+                    Console.WriteLine("Press enter");
+                    Console.ReadLine();
                     break;
                 }
                 case 4:
                 {
+                    int n = 5;
+                    string path = @"D:\Prog Proj\C#\base-dotnet-programs\Lab8\ElectronicPetitionLab8\ElectronicPetitionProgramLab8\Resources\semaphoretext.txt";
+                    FileReader instance = new FileReader(path);
                     
+                    for (int i = 0; i < n; i++)
+                    {
+                        Thread thread1 = new Thread(instance.WriteFileSemaphore) {Name = (i + 1) + ""};
+                        thread1.Start();
+                    }
+                    
+                    Console.WriteLine("Press enter");
+                    Console.ReadLine();
                     break;
                 }
             }
@@ -135,26 +160,7 @@ namespace ElectronicPetitionProgramLab8
             
             Console.WriteLine("Task 4.2:");
         }
-
-        public static void AddFileText()
-        {
-            textFile += $"Added Text {Thread.CurrentThread.Name} ";
-        }
         
-        public static void SynthronizedAddFileText()
-        {
-            lock (locker)
-            {
-                textFile += $"Added Text {Thread.CurrentThread.Name} ";
-            }
-        }
         
-        public static void MutexAddFileText()
-        {
-            mutexObj.WaitOne();
-            textFile += $"Added Text {Thread.CurrentThread.Name} ";
-            Thread.Sleep(100);
-            mutexObj.ReleaseMutex();
-        }
     }
 }
